ae535bc41b8e33fbc5f096154acf0e681d4fa815
斉藤崇 <tks@saitoutakashinoMacBook-Pro.local>
2020-08-20 19:38:29 +0900

    update 19:38 20 AUG

--- greeting.py
+++ greeting.py
@@ -1,8 +1,26 @@
-#print("Good Morning")
+print("Good Morning")
 
-#print("Good Afternoon")
+print("Good Afternoon")
 
-#print("Good Evening")
+print("Good Evening")
 
 num = 1
 print(num)
+
+a = 10
+b = 1
+bool01 = (a > b)
+print(type(bool01))
+print(bool01)
+
+a = ["saito","suzuki","takashi"]
+a[1] = "tanaka"
+
+print(a[0])
+print(a[2])
+print(a[1])
+
+data_box = ["takashi-0","takashi-1","takashi-2"]
+
+print(data_box[0])
+print(data_box[1])
